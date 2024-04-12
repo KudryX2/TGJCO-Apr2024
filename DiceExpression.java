24ff36b59df7bd770c3b7e23eaa44fe9cc1c2019
Kudry <kudry995@gmail.com>
2024-04-12 12:35:14 +0200

    methods added

--- DiceExpression.java
+++ DiceExpression.java
@@ -5,6 +5,21 @@ public class DiceExpression {
         this.expression = expression.trim(); // Trim the input string
     }
 
+    public boolean validate(String input){
+
+    }
+
+    public int evaluateDiceValue(String diceInput){
+
+    }
+
+    public int combineExpression(String partialExpression){
+
+    }
+
     public void evaluate() {
+
+
+
     }
 }
