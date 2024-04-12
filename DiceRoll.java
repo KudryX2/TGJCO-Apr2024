8b8f2b1835f5fc19bdcaed3392566b910169115f
Kudry <kudry995@gmail.com>
2024-04-12 12:18:10 +0200

    test commit

--- DiceRoll.java
+++ DiceRoll.java
@@ -3,7 +3,7 @@ public class DiceRoll {
     // Static method to roll a die with a specified number of sides
     public static int roll(int sides) {
         // Implement dice rolling logic
-        return 0;
+        return -1;
     }
 
 }
